4fe9448222d420c7b855c55946bfa7152abb1530
itmacleod1995 <imacleod@mail.umw.edu>
2020-06-04 22:23:07 -0400

    Finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,13 @@ public class Main {
 	// write your code here
     }
     public static int reverseInt(int num){
-        
+        int reverse = 0;
+
+        while(num != 0){
+            reverse = (reverse * 10) + num % 10;
+            num = num / 10;
+        }
+
+        return reverse;
     }
 }
