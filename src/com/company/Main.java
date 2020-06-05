33464dbec494fbc2a6f92c9f2503722275e28417
itmacleod1995 <imacleod@mail.umw.edu>
2020-06-04 22:25:51 -0400

    Added a test case

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println(reverseInt(987));
     }
     public static int reverseInt(int num){
         int reverse = 0;
